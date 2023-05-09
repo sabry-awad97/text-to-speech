c8cd0d34cf902d49490b7fd631f35efe7358960a
Sabry Awad <dr.sabry1997@gmail.com>
2023-05-09 09:38:34 +0300

    🚀 feat(main.rs): Add GoogleTranslateClient struct and build_url function To synthesize text using Google Translate Text-to-Speech (TTS), a new struct `GoogleTranslateClient` was created to handle the TTS API endpoint URL construction. The `build_url` function takes a string input and returns a `Result<String, Box<dyn Error>>` with the constructed API endpoint URL. Changes made: - `GoogleTranslateClient` struct added to handle TTS API endpoint URL construction. - `build_url` function added to construct TTS API endpoint URL. Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,39 @@ struct SynthesizeTextRequest {
     client: String,
 }
 
+struct GoogleTranslateClient {
+    client: Client,
+    base_url: String,
+    language: String,
+}
+
+impl GoogleTranslateClient {
+    fn new(language: String) -> Self {
+        let client = Client::new();
+        let base_url = String::from("https://translate.google.fr/translate_tts");
+
+        Self {
+            client,
+            base_url,
+            language,
+        }
+    }
+
+    fn build_url(&self, text: &str) -> Result<String, Box<dyn Error>> {
+        let request = SynthesizeTextRequest {
+            input_encoding: "UTF-8".to_owned(),
+            query: text.to_owned(),
+            target_language: self.language.to_owned(),
+            total: 1,
+            idx: 0,
+            textlen: text.len() as i32,
+            client: "tw-ob".to_owned(),
+        };
+
+        let query_string = serde_urlencoded::to_string(&request)?;
+        Ok(format!("{}?{}", self.base_url, query_string))
+    }
+}
 fn main() {
     println!("Hello, world!");
 }
