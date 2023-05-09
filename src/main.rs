2c4e3ed4095cd1c7f9dfea3cb820fd2e9168c5e9
Sabry Awad <dr.sabry1997@gmail.com>
2023-05-09 09:37:29 +0300

    ✨ feat(main.rs): Add struct for Google Text-to-Speech API request In this code diff, a new struct `SynthesizeTextRequest` has been added to represent the request data for the Google Text-to-Speech API. The struct is derived with the `Debug` and `Serialize` traits, and has fields for `input_encoding`, `query`, `target_language`, `total`, `idx`, and `textlen`. These fields are expected to be populated with values specific to the desired API request. Changes made: - A new struct `SynthesizeTextRequest` was added with fields to represent request data for Google Text-to-Speech API. Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use reqwest::{Client, Response};
+use serde::Serialize;
+use std::error::Error;
+use tokio::fs::File;
+use tokio::io::AsyncWriteExt;
+
+#[derive(Debug, Serialize)]
+struct SynthesizeTextRequest {
+    #[serde(rename = "ie")]
+    input_encoding: String,
+    #[serde(rename = "q")]
+    query: String,
+    #[serde(rename = "tl")]
+    target_language: String,
+    total: i32,
+    idx: i32,
+    textlen: i32,
+    client: String,
+}
+
 fn main() {
     println!("Hello, world!");
 }
